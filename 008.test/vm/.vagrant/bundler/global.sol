dcee10622d823835c95bbefed9a3f152a670fdc2
BorisPlus <borisov-ilia@yandex.ru>
2021-06-01 00:10:34 +0300

    * spawn

--- 008.test/vm/.vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.21.0"]]],"checksum":"a17c0554d18cedf5c223ac87beaf9d54f0e56b8bbb7d9150ae26d1395f27fec1","vagrant_version":"2.2.16"}
