bd9cf3d566b848ccc21e335eddee1c77dc4ec66f
BorisPlus <borisov-ilia@yandex.ru>
2021-08-03 21:34:53 +0300

    * 023

--- 015/notes.sh
+++ 015/notes.sh
@@ -1,5 +1,8 @@
+vagrant up
+vagrant ssh
 sudo su
-sudo useradd -D
+# check defaults settings
+# sudo useradd -D
 sudo useradd admin
 echo "test" | sudo passwd admin --stdin
 sudo useradd moderator -g admin
@@ -11,17 +14,61 @@ echo "test" | sudo passwd user002 --stdin
 sudo useradd user003
 echo "test" | sudo passwd sudo --stdin
 
-
-[root@hw015 vagrant]# cat /etc/pam.d/sshd | grep pam_nologin
-account    required     pam_nologin.so
-[root@hw015 vagrant]# cat /etc/pam.d/sshd | grep pam_time
-
-sed -i 's@pam_nologin.so@pam_nologin.so\naccount    required     pam_time.so@g' /etc/pam.d/sshd
+cat /etc/pam.d/sshd | grep pam_nologin
+    account    required     pam_nologin.so
 cat /etc/pam.d/sshd | grep pam_time
-account    required     pam_time.so
+
+if grep -q 'pam_time.so' /etc/pam.d/sshd
+then
+  echo "'pam_time.so' already exists in '/etc/pam.d/sshd'";
+else
+  sed -i 's@pam_nologin.so@pam_nologin.so\naccount    required     pam_time.so@g' /etc/pam.d/sshd;
+fi
+
+if grep -q 'account    required     pam_time.so' /etc/pam.d/sshd
+then
+  echo "Add module 'pam_time.so' to '/etc/pam.d/sshd' - DONE";
+fi
+
+# The times field is used to indicate the times at which this rule applies.
+# The format here is a logic list of day/time-range entries.
+# The days are specified by a sequence of two character entries,
+# MoTuSa for example is Monday Tuesday and Saturday.
+# Note that repeated days are unset MoMo = no day, and MoWk = all weekdays bar Monday.
+# The two character combinations accepted are Mo Tu We Th Fr Sa Su Wk Wd Al,
+# the last two being week-end days and all 7 days of the week respectively.
+# As a final example, AlFr means all days except Friday.
+
+if grep -q '# otus homework 015 conclusion' /etc/security/time.conf
+then
+  echo "'otus homework 015 conclusion' already exists in '/etc/security/time.conf'";
+else
+  echo "# otus homework 015 conclusion" >> /etc/security/time.conf;
+  awk -F':' '{print $1":"$4}' /etc/passwd | \
+  grep `awk -F':' '/^admin:/{print $3}' /etc/group` | \
+    awk -F':' '{print $1}' | \
+      sed ':b;$!{N;bb};s/\n/\&/g' | \
+        awk '{print "sshd;*;!"$0";AlSaSu\nsshd;*;!"$0";!Al\nlogin;*;!"$0";AlSaSu\nlogin;*;!"$0";!Al"}' \
+          >> /etc/security/time.conf # *;*;!|admin|moderator;AlStSn
+fi
+
+ssh admin@localhost
 
 
-cat /etc/group | grep ^admin:
+echo 'login ; * ; !admin ; MoTuWeThFr0800-2000' >> /etc/security/time.conf;
+echo 'sshd ; * ; !admin ; MoTuWeThFr0800-2000' >> /etc/security/time.conf;
+echo 'sshd ; * ; !admin|user001 ; MoTuWeThFr0800-2000' >> /etc/security/time.conf;
+echo 'login ; * ; !admin|user001 ; MoTuWeThFr0800-2000' >> /etc/security/time.conf;
+echo 'sshd ; * ; !(admin|user001) ; MoTuWeThFr0800-2000' >> /etc/security/time.conf;
+echo 'login ; * ; !(admin|user001) ; MoTuWeThFr0800-2000' >> /etc/security/time.conf;
+echo 'login ; * ; !(admin|user001) ; !Al0000-2400' >> /etc/security/time.conf;
+echo 'sshd;*;!admin&user001;AlSaSu' >> /etc/security/time.conf;
+echo 'sshd;*;!admin&user001;!Al' >> /etc/security/time.conf;
+login ; * ; !root ; !Al0000-2400 http
+cat /etc/pam.d/sshd | grep pam_time
+    account    required     pam_time.so
+
+# cat /etc/group | grep ^admin:
 
 awk -F':' '{print $1":"$4}' /etc/passwd | grep < awk -F':' '/^admin:/{print $3}' /etc/group
 grep '^admin:.*$' /etc/group | cut -d: -f4
@@ -29,25 +76,34 @@ grep '^admin:.*$' /etc/group | cut -d: -f4
 awk -F':' '{print $1":"$4}' /etc/passwd | grep `awk -F':' '/^admin:/{print $3}' /etc/group` | sed ':b;$2!{N;bb};s/\n/,/g'
 awk -F':' '{print $1":"$4}' /etc/passwd | grep `awk -F':' '/^admin:/{print $3}' /etc/group` | awk -F':' '{print $1}' | sed ':b;$2!{N;bb};s/\n/,/g'
  awk -F':' '{print $1":"$4}' /etc/passwd | grep `awk -F':' '/^admin:/{print $3}' /etc/group` | awk -F':' '{print $1}' | tr '\n' ',' |
- awk -F':' '{print $1":"$4}' /etc/passwd | grep `awk -F':' '/^admin:/{print $3}' /etc/group` | awk -F':' '{print $1}' |  awk '{print}' ORS=';'
+
+awk -F':' '{print $1":"$4}' /etc/passwd | grep \
+  `awk -F':' '/^admin:/{print $3}' /etc/group` | awk \
+  -F':' '{print $1}' | awk \
+  '{print}' ORS=';'
 
 
 echo '*;*;admin;Al'  >> /etc/security/time.conf
 awk -F':' '{print $1":"$4}' /etc/passwd | grep `awk -F':' '/^admin:/{print $3}' /etc/group` | awk -F':' '{print $1}' | sed ':b;$!{N;bb};s/\n/|/g' | awk '{print "*;*;!|"$0";AlStSn"}' >> /etc/security/time.conf
 cat /etc/security/time.conf | grep admin
 
-awk -F':' '{print $1":"$4}' /etc/passwd | grep `awk -F':' '/^admin:/{print $3}' /etc/group | awk -F':' '{print $1}' | sed ':b*;*;user001;$!{N;bb};s/\n/|/g '` >> /etc/security/time.conf
+awk -F':' '{print $1":"$4}' /etc/passwd | grep \
+  `awk -F':' '/^admin:/{print $3}' /etc/group | awk -F':' '{print $1}' | sed ':b*;*;user001;$!{N;bb};s/\n/|/g '` >> /etc/security/time.conf
 sudo su
 echo '*;*;admin;Al'  >> /etc/security/time.conf
 
 vagrant ssh login -u user002
 
+awk -F':' '{print $1":"$4}' /etc/passwd | grep  `awk -F':' '/^admin:/{print $3}' /etc/group` | awk   -F'|/g' | awk  '{print "*;*;!|"$0:' '{print $1}' | sed  \
+        ':b;$!{N;bb};s/\n/";AlStSn"}' \
+          >> /etc/security/time.conf
 
 awk: cmd. line:1: {print "*;*;!|"$0";AlStSn"
 awk: cmd. line:1:                           ^ unexpected newline or end of string
 [root@hw015 ~]# awk -F':' '{print $1":"$4}' /etc/passwd | grep `awk -F':' '/^admin:/{print $3}' /etc/group` | awk -F':' '{print $1}' | sed ':b;$!{N;bb};s/\n/|/g' | awk '{print "*;*;!|"$0";AlStSn"
 }' >> /etc/security/time.conf
-[root@hw015 ~]# awk -F':' '{print $1":"$4}' /etc/passwd | grep `awk -F':' '/^admin:/{print $3}' /etc/group` | awk -F':' '{print $1}' | sed ':b;$!{N;bb};s/\n/|/g' | awk '{print "*;*;!|"$0";AlStSn"}' >> /etc/security/time.conf
+[root@hw015 ~]#
+
 [root@hw015 ~]# cat^C
 [root@hw015 ~]#
 [root@hw015 ~]# echo '*;*;admin;Al'  >> /etc/security/time.conf
