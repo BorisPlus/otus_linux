e4fd78db45d33eaf4e966a937cbbdb2c454c5f80
BorisPlus <borisov-ilia@yandex.ru>
2021-08-16 03:47:38 +0300

    * 026

--- 026/000/app.py
+++ 026/000/app.py
@@ -3,11 +3,12 @@ import ipaddress
 
 if __name__ == '__main__':
 
-    nw = ipaddress.ip_network('192.168.1.192/26')
+    nw = ipaddress.ip_network('192.168.0.0/16')
     print(nw.network_address)
     for h in nw.hosts():
         print(h)
-    print(nw.broadcast_address)
+    print('broadcast_address', nw.broadcast_address)
+    print('network_address', nw.network_address)
     print(nw.num_addresses)
     print(len(set(nw.hosts())))
 
