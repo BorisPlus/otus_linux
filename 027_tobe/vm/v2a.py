b67e49c02d1c4c86bd2d791e7d3e6efebb36089f
BorisPlus <borisov-ilia@yandex.ru>
2021-08-23 02:45:56 +0300

    * 027 ansible

--- 027_tobe/vm/v2a.py
+++ 027_tobe/vm/v2a.py
@@ -1,6 +1,7 @@
 from vagranttoansible import vagranttoansible
 
 # python3 v2a.py -o ../ansible/inventories/hosts
+vagranttoansible.DEFAULT_LINE_FORMAT = f"{vagranttoansible.DEFAULT_LINE_FORMAT} ansible_ssh_transfer_method=scp"
 # vagranttoansible.DEFAULT_LINE_FORMAT = "[{host}]\n" \
 #                                        "{host} " \
 #                                        "ansible_user={user} " \
