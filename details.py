84ed9dc305ec029b1dafa588c8901b365a8460e5
BorisPlus <borisov-ilia@yandex.ru>
2021-09-27 03:54:55 +0300

    * empty

--- details.py
+++ details.py
@@ -56,25 +56,25 @@ if __name__ == '__main__':
                     with open(file_in_tmp) as f_in_tmp:
                         rows_count = len(f_in_tmp.readlines())
                         if not file_in_tmp.endswith('.md'):
-                            if rows_count > rows_count_detail_limit:
+                            if rows_count > rows_count_detail_limit or rows_count == 0:
                                 rows_to_write.append(f'<details><summary>см. {screen_name}</summary>\n')
                                 rows_to_write.append('\n')
-                    if rows_count:
-                        if file_in_tmp.endswith('.sh'):
-                            rows_to_write.append('```')
-                            rows_to_write.append('shell')
-                        elif file_in_tmp.endswith('.py'):
-                            rows_to_write.append('```')
-                            rows_to_write.append('python3')
-                        elif file_in_tmp.endswith('.md'):
-                            ...
-                        elif file_in_tmp.endswith('.yml'):
-                            rows_to_write.append('```')
-                            rows_to_write.append('properties')
-                        else:
-                            rows_to_write.append('```')
-                            rows_to_write.append('text')
-                        rows_to_write.append('\n')
+                    # if rows_count:
+                    if file_in_tmp.endswith('.sh'):
+                        rows_to_write.append('```')
+                        rows_to_write.append('shell')
+                    elif file_in_tmp.endswith('.py'):
+                        rows_to_write.append('```')
+                        rows_to_write.append('python3')
+                    elif file_in_tmp.endswith('.md'):
+                        ...
+                    elif file_in_tmp.endswith('.yml'):
+                        rows_to_write.append('```')
+                        rows_to_write.append('properties')
+                    else:
+                        rows_to_write.append('```')
+                        rows_to_write.append('text')
+                    rows_to_write.append('\n')
 
                     with open(file_in_tmp) as f_in_tmp_2:
                         for row_in_tmp in f_in_tmp_2:
@@ -83,14 +83,14 @@ if __name__ == '__main__':
                     if file_in_tmp.endswith('.md'):
                         ...
                     else:
-                        if rows_count:
-                            rows_to_write.append('\n```\n')
+                        # if rows_count:
+                        rows_to_write.append('\n```\n')
 
-                    if rows_count:
-                        rows_to_write.append('\n')
+                    # if rows_count:
+                    rows_to_write.append('\n')
 
                     if not file_in_tmp.endswith('.md'):
-                        if rows_count > rows_count_detail_limit:
+                        if rows_count > rows_count_detail_limit or rows_count == 0:
                             rows_to_write.append('</details>')
                             rows_to_write.append('\n')
 
